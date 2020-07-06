cb964c63c1caf3198738f8893edb9b8824a9ec8c
JamesChavess <alfonsoforero1022@gmail.com>
2020-07-06 10:53:56 -0500

    separation of folders for multiple read alongs

---
+++ true History month Feb. 2018/src/definitions.js
@@ -0,0 +1,58 @@
+
+function swahili() {
+  alert(
+    "swahili es un idioma africano de la region de Congo"
+  );
+}
+function ultimately() {
+  alert(" Utilizado comunmente para expresar una conculsion. Al fin o finalmente");
+}
+function than() {
+  alert(
+    "than es usado para presentar la segunda parte de una comparasion"
+  );
+}
+function theoredical() {
+  alert(
+    " enfocado en la theoria de algo en ;ugar de su aplicacion parctica "
+  );
+  preventDefault();
+}
+function exchangeOfKnowledge() {
+  alert(
+    "El intercambio de conocimientos es cualquier proceso a traves del cual se comparten ideas, evidencias, experiencias y habilidades para un mejor conocimiento bidireccional"
+  );
+}
+function slag() {
+  alert(
+    " voccabulario, expresiones o dichos utilizados entre personas de la misa demographica para expresr algo que entre ellos entienden, ejemplo : 'no mames guey'"
+  );
+}
+function sharp() {
+  alert(
+    "sharp se ignifica Agudo o filudo cuando hablas de objetos. hablando de seres humanos se ignifica ser astuto."  
+  );
+}
+function seemed() {
+  alert(
+    " la impresion de que algo es de alguna manera, o tiene alguna cualidad o caracteristica en particular"
+  );
+}
+function guru() {
+  alert(
+    " Un maestro influyente y popularmente conocido como experto"
+  );
+}
+function hoard() {
+  alert(
+    "acumular para uso propio, aveses ambisiosamente a escondidas"
+  );
+}
+function james() {
+  alert(
+    "Por razones de tradicion Biblica y origenes en el lenguaje 'latin', el nombre 'James' traduce a 'Satiago' en espanol "
+  );
+  preventDefault();
+}
+
+
